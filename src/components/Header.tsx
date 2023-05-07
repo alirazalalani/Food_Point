0acc87f7124812f2078502d8bab9cb63b567fab6
alirazalalani <aliraza.lalani12@gmail.com>
2023-05-08 00:13:31 +0500

    feat ( signIn screen implemented )

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {COLORS, PARAMETERS} from '../global/styles';
+import {Icon} from '@rneui/themed';
+import {horizontalScale, verticalScale} from '../utils/Dim';
+const Header = ({title,type}: any) => {
+  return (
+    <View style={styles.header}>
+      <Icon
+        type="material-community"
+        name={type}
+        color={COLORS.HeaderText}
+        size={28}
+        onPress={() => {}}
+      />
+      <Text style={styles.headerText}>{title}</Text>
+    </View>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: COLORS.buttons,
+    height: PARAMETERS.headerHeight,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: horizontalScale(20),
+  },
+  headerText: {
+    color: COLORS.HeaderText,
+    fontSize: verticalScale(22),
+    fontWeight: 'bold',
+    marginLeft: horizontalScale(25),
+  },
+});
